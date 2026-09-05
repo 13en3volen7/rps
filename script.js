91ec6bd6f3c6af632320b28ca44ff216e841162d
13en3volen7 <64196796+13en3volen7@users.noreply.github.com>
2026-09-05 23:05:56 +0700

    Implement game logic & game loop

--- script.js
+++ script.js
@@ -9,3 +9,56 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Enter your choice:").toLowerCase();
 }
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === "rock") {
+            if (computerChoice === "paper") {
+                computerScore++;
+                console.log("You lose! Paper beats Rock");
+            } else if (computerChoice === "scissor") {
+                humanScore++;
+                console.log("You win! Rock beats Scissor");
+            } else {
+                console.log("Tie! Both chose Rock");
+            }
+        } else if (humanChoice === "paper") {
+            if (computerChoice === "scissor") {
+                computerScore++;
+                console.log("You lose! Scissor beats Paper");
+            } else if (computerChoice === "rock") {
+                humanScore++;
+                console.log("You win! Paper beats Rock");
+            } else {
+                console.log("Tie! Both chose Paper");
+            }
+        } else if (humanChoice === "scissor") {
+            if (computerChoice === "rock") {
+                computerScore++;
+                console.log("You lose! Rock beats Scissor");
+            } else if (computerChoice === "paper") {
+                humanScore++;
+                console.log("You win! Scissor beats Paper");
+            } else {
+                console.log("Tie! Both chose Paper");
+            }
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    if (humanScore === computerScore) {
+        console.log(`The game has ended in a tie!`);
+    } else {
+        console.log(`The game has ended! The winner is ${(humanScore > computerScore) ? "human" : "computer"}!`);
+    }
+}
+
+playGame();
