c35981fbd0ecdafc283d59e597880845aad0228d
13en3volen7 <64196796+13en3volen7@users.noreply.github.com>
2026-09-05 23:03:31 +0700

    Implement getComputerChoice function

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+    switch (Math.floor(Math.random() * 10) % 3) {
+        case 0: return "rock";
+        case 1: return "paper";
+        case 2: return "scissor";
+    };
+}
